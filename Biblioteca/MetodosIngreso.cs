ffb74515b74fe7f293591a828dce6b441017f6ad
moreescudero <escuderomore1@gmail.com>
2022-09-06 01:07:19 -0300

    ej labo

--- Biblioteca/MetodosIngreso.cs
+++ Biblioteca/MetodosIngreso.cs
@@ -32,18 +32,132 @@ namespace Biblioteca
         }
         public static int IngresarInt(string? mensaje, int maximo, int minimo)
         {
-            int i = IngresarInt(mensaje);
-            while(i < minimo && i > maximo)
+            int numero = IngresarInt(mensaje);
+            while(numero < minimo || numero > maximo)
             {
                 Console.WriteLine("ERROR");
-                i = IngresarInt(mensaje);
+                numero = IngresarInt(mensaje);
             }
-            return i;
+            return numero;
+        }
+        
+        
+        public static float IngresarFloat(string? mensaje)
+        {
+            float numero = 0;
+            bool esNumerica;
+            string? numeroIngresado;
+            do
+            {
+                Console.WriteLine(mensaje);
+                numeroIngresado = Console.ReadLine();
+                esNumerica = EsNumerica(numeroIngresado);
+                if(esNumerica)
+                {
+                    float.TryParse(numeroIngresado, out numero);
+                }
+                else
+                {
+                    Console.WriteLine("ERROR");
+                }
+            } while (!esNumerica);
+
+            return numero;
+        }
+        public static float IngresarFloat(string? mensaje, int maximo, int minimo)
+        {
+            float numero = IngresarFloat(mensaje);
+            while(numero < minimo || numero > maximo)
+            {
+                Console.WriteLine("ERROR");
+                numero = IngresarFloat(mensaje);
+            }
+            return numero;
         }
 
         private static bool EsNumerica(string? numIngresado)
         {
-            return int.TryParse(numIngresado, out int numero);
+            return float.TryParse(numIngresado, out float numero);
+        }
+
+        private static bool ValidarLetra(string? letra)
+        {
+            bool retorno = false;
+
+            if (letra is not null)
+            {
+                for (int i = 0; i < letra.Length; i++)
+                {
+                    if (char.IsLetter(letra[i]))
+                    {
+                        retorno = true;
+                    }
+                    else
+                    {
+                        retorno = false;
+                        break;
+                    }
+                }
+            }
+
+            return retorno;
+        }
+        
+        private static bool ValidarAlfaNumerica(string? letra)
+        {
+            bool retorno = false;
+
+            if (letra is not null)
+            {
+                for (int i = 0; i < letra.Length; i++)
+                {
+                    if (char.IsLetterOrDigit(letra[i]))
+                    {
+                        retorno = true;
+                    }
+                    else
+                    {
+                        retorno = false;
+                        break;
+                    }
+                }
+            }
+
+            return retorno;
+        }
+
+        
+        public static string IngresarAlfaNumerica(string? mensaje)
+        {
+            string? palabra;
+            do
+            {
+                Console.WriteLine(mensaje);
+                palabra = Console.ReadLine();
+                if (!ValidarAlfaNumerica(palabra))
+                {
+                    Console.WriteLine("ERROR");
+                }
+            } while (!ValidarAlfaNumerica(palabra));
+
+            return palabra;
+        }
+
+        
+        public static string IngresarString(string? mensaje)
+        {
+            string? palabra;
+            do
+            {
+                Console.WriteLine(mensaje);
+                palabra = Console.ReadLine();
+                if(!ValidarLetra(palabra))
+                {
+                    Console.WriteLine("ERROR");
+                }
+            } while (!ValidarLetra(palabra));
+
+            return palabra;
         }
     }
 }
