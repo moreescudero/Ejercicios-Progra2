49b69184ba3b643020b7c45728af8f2c036d0cbe
moreescudero <escuderomore1@gmail.com>
2022-09-08 00:05:29 -0300

    ej 3-2

---
+++ Biblioteca/Persona.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Persona
+    {
+        string? nombre;
+        DateTime fechaDeNacimiento;
+        int dni;
+
+        public Persona(string? nombre, DateTime fechaDeNacimiento, int dni)
+        {
+            this.nombre = nombre;
+            this.fechaDeNacimiento = fechaDeNacimiento;
+            this.dni = dni;
+        }
+
+        public string Nombre
+        {
+            get { return nombre;  }
+            set { nombre = value; }
+        }
+
+        public DateTime FechaDeNacimiento
+        {
+            get { return fechaDeNacimiento; }
+            set { fechaDeNacimiento = value; }
+        }
+
+        public int Dni
+        {
+            get { return dni; }
+            set { dni = value; }
+        }
+
+        private int CalcularEdad()
+        {
+            DateTime ahora = DateTime.Now;
+            return (ahora - FechaDeNacimiento).Days / 365; ;
+        }
+
+        private string EsMayorDeEdad(int edad)
+        {
+            if(edad >= 18)
+            {
+                return "Es mayor de edad";
+            }
+            return "Es menor de edad";
+        }
+
+        public string Mostrar()
+        {
+            int edad = CalcularEdad();
+            string? mensaje = "Nombre: " + Nombre + "\nFecha nacimiento: " + FechaDeNacimiento.ToString("d") + "\nDNI: " + Dni + "\nEdad: " + edad + "\n" + EsMayorDeEdad(edad);
+
+            return mensaje;
+        }
+    }
+}
---
+++ Ejercicio3-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using Biblioteca;
+
+namespace Ejercicio3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Vos cuantas primaveras tenes?");
+
+            Persona primerPersona = new Persona("Morena", new DateTime(2001,6,15), 9999999);
+            Persona segundaPersona = new Persona("Matias", new DateTime(1998, 11, 10), 1010101);
+            Persona tercerPersona = new Persona("Mishka", new DateTime(2016, 11, 30), 3333333);
+
+            Persona[] personas = { primerPersona, segundaPersona, tercerPersona };
+
+            foreach(Persona persona in personas)
+            {
+                Console.WriteLine(persona.Mostrar() + "\n");
+            }
+        }
+    }
+}
