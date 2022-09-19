d87c89209fd9f9d0cb2b5228867811e57d39ce8d
moreescudero <escuderomore1@gmail.com>
2022-09-19 20:02:05 -0300

    Polimorfismo 19/9

---
+++ EjercicioClase19-9/Sobrescrito.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase19_9
+{
+    public class Sobrescrito
+    {
+        public override string ToString()
+        {
+            return "¡Este es mi metodo ToString sobrecargado!";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            Sobrescrito sobrescrito = obj as Sobrescrito;
+            if(sobrescrito is not null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1142510181;
+        }
+    }
+}
